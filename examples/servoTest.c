3117c1a01612cf94dc71e6b77e3c92cd52912f34
Jules Dourlens <jdourlens@gmail.com>
2013-10-28 01:15:05 +0100

    Servo code

--- examples/servoTest.c
+++ examples/servoTest.c
@@ -1,14 +1,16 @@
-#include "firmata.h"
+#include "servo.h"
 
 int		main()
 {
   t_firmata	*firmata;
+  t_servo	*servo;
   int		i = 0;
 
   firmata = firmata_new("/dev/ttyACM0");
   while(!firmata->isReady)
     firmata_pull(firmata);
-  firmata_pinMode(firmata, 3, MODE_SERVO);
+  sleep(1);
+  servo = servo_attach(firmata, 3);
   while (1)
     {
       firmata_pull(firmata);
@@ -16,7 +18,7 @@ int		main()
       usleep(10000);
       if (i == 180)
 	i = 0;
-      firmata_analogWrite(firmata, 3, i);
+      servo_write(servo, i);
     }
 }
 
