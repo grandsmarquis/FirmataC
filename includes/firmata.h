91ffac93b2f80b5430e51bd887334062ebdcb870
jdourlens <jdourlens@gmail.com>
2015-09-21 19:37:20 +0200

    New license under MIT

--- includes/firmata.h
+++ includes/firmata.h
@@ -1,20 +1,3 @@
-/*  FirmataC Firmata                                                            
- *  Copyright 2013, Jules Dourlens (jdourlens@gmail.com)                       
- *  Mainly based on Firmata GUI-Friendly by Paul Stoffregen (paul@pjrc.com)    
- *                                                                             
- *  This program is free software: you can redistribute it and/or modify       
- *  it under the terms of the GNU General Public License as published by       
- *  the Free Software Foundation, either version 3 of the License, or          
- *  (at your option) any later version.                                        
- *                                                                             
- *  This program is distributed in the hope that it will be useful,            
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of             
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              
- *  GNU General Public License for more details.                               
- *                                                                             
- *  You should have received a copy of the GNU General Public License          
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.      
- */
 
 #ifndef		__H_FIRMATAC_
 #define		__H_FIRMATAC_
